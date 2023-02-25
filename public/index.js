15c17de9b21f95fd6e725617d08f9db227fed2df
Georgy Grechko <georgre4ko@ya.ru>
2023-02-25 03:01:50 +0300

    Добавил `handlebars` и общую структуру проекта

--- public/index.js
+++ public/index.js
@@ -1,5 +1,42 @@
-const p = document.getElementsByClassName('header')[0];
+import Router from './router/router.js';
 
-p.addEventListener('click', () => {
-    p.style.color = 'beer';
-})
+(function () {
+    const router = new Router(null, 'app', '/');
+
+    fetch('./views/main_page/main_page.handlebars').then((val) => {
+        val.text().then((val) => {
+            // eslint-disable-next-line no-undef
+            let mainTemplate = Handlebars.compile(val);
+            var html = mainTemplate({ time: new Date().toTimeString() });
+
+            const app = document.getElementById('app');
+            app.innerHTML = html;
+        });
+    });
+
+    fetch('./views/menu/menu.handlebars').then((val) => {
+        val.text().then((val) => {
+            // eslint-disable-next-line no-undef
+            let menuTemplate = Handlebars.compile(val);
+            var html = menuTemplate({
+                menu_items: [
+                    { link: '/profile', name: 'Профиль' },
+                    { link: '/feed', name: 'Лента' },
+                    { link: '/chat', name: '<p>Чат</p>' },
+                ],
+            });
+
+            const menu = document.getElementById('menu');
+            menu.innerHTML = html;
+
+            const links = document.getElementsByTagName('a');
+
+            for (let i = 0; i < links.length; i++) {
+                links[i].addEventListener('click', (e) => {
+                    e.preventDefault();
+                    router.goToRoute(links[i].getAttribute('href'));
+                });
+            }
+        });
+    });
+})();
